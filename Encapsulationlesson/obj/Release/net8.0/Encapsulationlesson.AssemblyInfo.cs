5033ae2d8ce4b9f7382e19f6fde0ab87ed3c845f
Amadeu Lopes <amadeutito83@gmail.com>
2024-05-30 11:04:58 +0100

    New class created

--- Encapsulationlesson/obj/Release/net8.0/Encapsulationlesson.AssemblyInfo.cs
+++ Encapsulationlesson/obj/Release/net8.0/Encapsulationlesson.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Encapsulationlesson")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4005b9ab7ef80fa5820e53e4004d910b607ee619")]
 [assembly: System.Reflection.AssemblyProductAttribute("Encapsulationlesson")]
