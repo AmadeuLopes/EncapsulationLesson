1a9ccb6d31c9d9c5a6972b4148cd7a7502699fe0
Amadeu Lopes <amadeutito83@gmail.com>
2024-05-30 10:18:27 +0100

    Lesson of Encapsulation studied

---
+++ Encapsulationlesson/Product.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Encapsulationlesson
+{
+     class Product
+    {
+        private string _name;
+        public Double Price { get; private set; }
+        public int Quantity { get; private set; }
+
+        public Product(string name, double price, int quantity) : this(name, price)
+        {
+
+            Quantity = quantity;
+        }
+        public Product(string name, Double price) : this()
+        {
+            _name = name;
+            Price = price;
+
+        }
+        public Product()
+        {
+            Quantity = 10;
+        }
+        public string Name
+        {
+            get { return _name; }
+            set {
+                if (value != null && value.Length > 1)
+                {
+                    _name = value;
+                }
+            }
+            
+        }
+        /*public Double Price
+        {
+            get { return _price; }
+        }
+        public int Quantity
+        {
+            get { return _quantity; }
+        }*/
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetName(string name)
+        {
+            if (name != null && name.Length > 1)
+            {
+                _name = name;
+            }
+            
+        }
+        /*public Double GetPrice()
+        {
+            return _price;
+        }
+
+        public int GetQuantity()
+        {
+            return _quantity;
+        }*/
+
+        public Double TotalValueStock()
+        {
+            return Price * Quantity;
+        }
+        public void AddProduct(int quantity)
+        {
+            Quantity += quantity;
+        }
+        public void RemoveProduct(int quantity)
+        {
+            Quantity -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return _name + ", $" + Price.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantity + " unidades, Total value: $" + TotalValueStock();
+        }
+    }
+}
---
+++ Encapsulationlesson/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Encapsulationlesson;
+
+Product pd=new Product("TV",900.00,10);
+
+pd.Name="TV 4K";
+
+Console.WriteLine(pd.Name);
+Console.WriteLine(pd.Price);
+Console.WriteLine(pd.Quantity);
